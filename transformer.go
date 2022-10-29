b996ab44195396aa0bcefe0e0d0c608285ec9d1c
Комолин Иван Александрович <ivan.kolomin@vseinstrumenti.ru>
2022-10-29 21:05:10 +0400

    fix

--- transformer.go
+++ transformer.go
@@ -1,9 +1,9 @@
 package transformer
 
 func Slice[In, Out any](items []In, f func(In) Out) []Out {
-	result := make([]Out, len(items), len(items))
-	for key, value := range items {
-		result[key] = f(value)
+	result := make([]Out, len(items))
+	for i, item := range items {
+		result[i] = f(item)
 	}
 
 	return result
